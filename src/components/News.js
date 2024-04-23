fb4d9e05a9d36bbc3bbd18d0bf3e1c34701c8811
ashish <adhsashish125@gmail.com>
2024-04-23 14:42:51 +0545

    added news item and news js component

--- src/components/News.js
+++ src/components/News.js
@@ -4,13 +4,21 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>This is news component
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
+      <div className='container my-3'>
+        <h2>Donkey News</h2>
+        <div className="row">
+          <div className="col-md-4">
+          <NewsItem title="Mytitle" description="myDescription" imageUrl="https://i.cbc.ca/1.7179497.1713559032!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_620/russia-ukraine-war-washington.jpg"/>
+          </div>
+          <div className="col-md-4">
+          <NewsItem title="Mytitle" description="myDescription"/>
+          </div>
+          <div className="col-md-4">
+          <NewsItem title="Mytitle" description="myDescription"/>
+          </div>
+        </div>
+        
+        
       </div>
       
     )
